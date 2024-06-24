f22debf16c0c8292cc4a40877532c75a454172dd
nikitinale7na <nikitina.gby@gmail.com>
2024-06-24 21:41:38 +0300

    Add files via upload

---
+++ domashka1.py
@@ -0,0 +1,5 @@
+first_name= 'Курс "Python"'
+homework_amount = 12
+number_of_hours = 1.5
+time_for_one_task = (number_of_hours/homework_amount)
+print(first_name, ', ', 'всего задач',':',  homework_amount,',', 'затрачено часов',':', number_of_hours,',', 'среднее время выполнения', time_for_one_task)
