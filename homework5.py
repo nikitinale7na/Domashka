45becadefd8ce556fb7a25959eef049cdb096709
nikitinale7na <nikitina.gby@gmail.com>
2024-07-02 15:07:25 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,8 +1,7 @@
-a= 1, 2, 3, 'apple', True
-print(a)
-print(type(a))
-print(a)
-a= [1, 2, 3], 'apple', True
-print (a)
-a[0][0] = 10
-print(a)
+immutable_var = 1, 2, 3, 'apple', True
+print(immutable_var)
+print(type(immutable_var))
+print(immutable_var)
+checklist= [1, 2, 3, 4, 5, 'apple', True]
+checklist[0] = 10
+print(checklist)
