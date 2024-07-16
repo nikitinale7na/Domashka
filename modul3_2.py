a80c8b947ab090b6f15e675385425ade3d82a587
nikitinale7na <nikitina.gby@gmail.com>
2024-07-16 21:10:55 +0300

    Update modul3_2.py

--- modul3_2.py
+++ modul3_2.py
@@ -1,9 +1,9 @@
 def send_email(message, recipient, sender='university.help@gmail.com'):
-        if (('@' and ('.com' or '.ru' or '.com')) not in (recipient and sender) or ('@' or ('.com' or '.ru' or '.net'))
+        if sender == recipient or recipient == sender:
+            print("Нельзя отправить письмо самому себе!")
+        elif (('@' and ('.com' or '.ru' or '.com')) not in (recipient and sender) or ('@' or ('.com' or '.ru' or '.net'))
         not in (recipient and sender)):
             print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-        elif recipient == sender or recipient == 'university.help@gmail.com':
-            print("Нельзя отправить письмо самому себе!")
         elif sender != 'university.help@gmail.com':
             print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
         else:
