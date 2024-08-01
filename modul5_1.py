5d3c5d4b35e62ef1642f56adacd59c21c2f894fe
nikitinale7na <nikitina.gby@gmail.com>
2024-08-01 21:20:17 +0300

    Add files via upload

---
+++ modul5_1.py
@@ -0,0 +1,21 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if 1 <= new_floor <= self.number_of_floors:
+                print(i)
+            else:
+                print('Такого этажа не существует')
+                break
+
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+h2.go_to(10)
+
