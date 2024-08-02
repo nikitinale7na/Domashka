3b0b0c061c68e6785bcd7bffb454a0209f9e45b7
nikitinale7na <nikitina.gby@gmail.com>
2024-08-02 22:32:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.9>9.8 or 1000!=1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# 4th program
+print(type(str('123.456')))
+a = str('123.456')
+print(a[4])
+print(int(123.456 * 10) - 1230)
+
+
+
+
