0e9df56c03a4593ce511209a887010d225791096
nikitinale7na <nikitina.gby@gmail.com>
2024-08-04 00:44:54 +0300

    Add files via upload

---
+++ modul5_4.py
@@ -0,0 +1,27 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
